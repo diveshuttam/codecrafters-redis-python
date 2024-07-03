8f405068b733dc81605488fdc18a991dc9d65987
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2024-07-03 23:50:48 +0530

    stage 5

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
+
 
 
 def main():
@@ -15,13 +17,18 @@ def main():
         client_socket, addr = server_socket.accept()
         # read packets from client_socket and send responses
         print("Connection from", addr)
-        while True:
-            data = client_socket.recv(1024)
-            print("Received", data)
-            if not data:
-                break
-            client_socket.sendall(b"+PONG\r\n")
 
+        def handle_client(client_socket):
+            while True:
+                data = client_socket.recv(1024)
+                print("Received", data)
+                if not data:
+                    break
+                client_socket.sendall(b"+PONG\r\n")
+        
+        # handle client in a separate thread
+        thread  = threading.Thread(target=handle_client, args=(client_socket,)).start()
+        thread.join() 
 
 if __name__ == "__main__":
     main()
