31781b07068b423f9453aaa2d1e881d681020fe5
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2024-07-04 14:00:44 +0530

    app

--- app/main.py
+++ app/main.py
@@ -188,6 +188,8 @@ class RedisServer:
         response = bytes(fullresync_response, 'utf-8')
         # Send the empty RDB file to the replica
         response += self._send_empty_rdb()
+        
+        response += "*3\r\n$8\r\nreplconf\r\n$6\r\ngetack\r\n$1\r\n*\r\n"
 
         # Connect to the slave
         # self._connect_to_slaves()
