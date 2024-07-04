b105e5d25d1f84e8a71389946b7853e26b045d26
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2024-07-04 15:56:52 +0530

    app

--- app/main.py
+++ app/main.py
@@ -231,7 +231,7 @@ class RedisServer:
             print("lines", lines)
             print("total terms", total_terms)
             command = lines[2].upper()
-            args = lines[2:total_terms*2+1:2]
+            args = lines[2:total_terms*2+1:2][1:]
             rest = lines[total_terms*2+1:]
             print("args: ", args)
             print("rest: ", rest)
